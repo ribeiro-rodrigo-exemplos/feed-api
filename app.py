d55a951c9748225d0892c344785839cacc26724f
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2018-06-06 21:58:48 -0300

    adicionando versionamento nos recursos da api

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ import os
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Feed, '/feeds')
+api.add_resource(Feed, '/v1/feeds')
+api.add_resource(User, '/v1/users')
 api.add_resource(Auth, '/auth/token')
-api.add_resource(User, '/users')
 
 if __name__ == '__main__':
     port = os.environ['PORT'] if 'PORT' in os.environ else 5000
